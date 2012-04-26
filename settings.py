b36d605e611542bd17779e5ccf8123e1c9720348
Ilya Bezrukov <rbetra@gmail.com>
2012-04-26 17:53:57 +0400

    add django-debug-toolbar

--- settings.py
+++ settings.py
@@ -120,7 +120,27 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+
+    #last
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
+
+#Django Debug Toolbar
+INTERNAL_IPS = ('127.0.0.1',)
+
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
+
+
 
 ROOT_URLCONF = 'source.urls'
 
@@ -147,6 +167,7 @@ INSTALLED_APPS = (
     'localeurl',
     #'nani',
     'modeltranslation',
+    'debug_toolbar',
 
     'home',
     'main',
